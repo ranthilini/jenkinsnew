f95bbfe84f4973dbfe7ecf54cd3c7b5e2f68fb1d
ranthilini <prarthana2ranthilini@gmail.com>
2024-09-30 11:12:21 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './ch.css';
+import img1 from './landing.png';
+import logo from './logo.png';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+
+
+<header className="app-bar">
+<img src={logo} alt="Company Logo" className="navbar-logo" style={{ width: "40px", height: "40px", marginRight: "20px" }} />
+
+<h1 className="navbar-company-name" style={{ fontSize: "1.2rem", fontWeight: "bold",  marginright: "5px" ,color:'white'}}>I Paid For You</h1>
+      <nav className="menu-links">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="/feedbackui">Feedback</a></li>
+          <li><a href="/requestt">Request</a></li>
+          <li><a href="/replyt">IPFU Manager</a></li>
+        </ul>
+      </nav>
+    </header>
+
+
+
+
+      <img
+        className="untitled-design-3-1"
+        alt=""
+        src={img1}
+        style={{ marginTop: '20px' }} />
+      <b className="share-your-thoughts" style={{ fontSize: '90px', whiteSpace: 'nowrap', paddingTop: '30px' }}>Share Your Thoughts</b>
+      <div className="feedback-page-child" style={{ paddingTop: '5px' }}></div>
+      <button className="submit-feedback" style={{ border: 'none', textAlign: 'center', fontSize: '18px', padding: '30px 30px 15px', display: 'block', margin: '0 auto' }} variant="contained" onClick={() => navigate('/feedback')}>
+        <span style={{ textAlign: 'center' }}>Submit Feedback</span>
+      </button>
+      <button className="submit-request" style={{ border: 'none', textAlign: 'center', fontSize: '18px', padding: '30px 20px 10px', display: 'block', margin: '0 auto' }} variant="contained" onClick={() => navigate('/request')}>
+        <span style={{ textAlign: 'center' }}>Submit Request</span>
+      </button>
+      <b className="help-us-improve" style={{ fontSize: '60px', whiteSpace: 'nowrap', paddingTop: '40px' }}>Help us improve donations</b>
     </div>
   );
-}
+};
 
 export default App;
